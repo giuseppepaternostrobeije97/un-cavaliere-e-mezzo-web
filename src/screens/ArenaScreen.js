334c7ed604581eedc642f7a3459ff893fd022da3
Riccardo-Perrone <riccardo.perrone@beije.it>
2022-12-01 14:33:03 +0100

    navigate exit lobby

--- src/screens/ArenaScreen.js
+++ src/screens/ArenaScreen.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import { Arena } from "un-cavaliere-e-mezzo-library";
+import { useNavigate } from "react-router-dom/dist";
 
 const ArenaScreen = () => {
   const location = useLocation();
+  const navigate = useNavigate();
+
+  function handleGame() {
+    navigate("/game");
+  }
+  function exitLobby() {
+    navigate("/lobby");
+  }
 
   return (
     <div className="container">
-      <Arena lobby={location.state} />
+      <Arena lobby={location.state} game={handleGame} exitLobby={exitLobby} />
     </div>
   );
 };
